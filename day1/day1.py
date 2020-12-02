f96ab7a4989203bf8ddbf2f742cb0435f3e14c87
Oscar Russo <odrusso1@gmail.com>
2020-12-02 21:28:22 +1300

    Day 1! Love a O(n^2) solution

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,8 @@
 # nothing here
+
+listIn = [int(_) for _ in open("in").readlines()]
+
+for num1, num2 in [(x, y) for x in listIn for y in listIn]:
+    # print(f"testing {num1} and {num2}")
+    if num1 + num2 == 2020:
+        print(f"{num1} {num2} {num1 * num2}")
