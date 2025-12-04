5b6be3f83bdf5e08e5c97ebea1df7181b610adf6
oscar <odrusso1@gmail.com>
2025-12-04 15:38:03 +0100

    day 3, without overthinking it

--- src/AdventOfCode2025/Day3/AboutDay3.cs
+++ src/AdventOfCode2025/Day3/AboutDay3.cs
@@ -22,15 +22,19 @@ public class AboutDay3
 
         Assert.AreEqual(357L, result);
     }
-    
-    [TestMethod]
-    public void PartOne_InvalidCase()
+
+    [DataTestMethod]
+    [DataRow("987654321111111", 98L)]
+    [DataRow("811111111111119", 89L)]
+    [DataRow("234234234234278", 78L)]
+    [DataRow("818181911112111", 92L)]
+    public void PartOne_IndividualCases(string bank, long expected)
     {
-        var input = new[] { "234234234234278" };
+        var input = new[] { bank };
 
         var result = new Day3().ProcessPartOne(input);
 
-        Assert.AreEqual(78L, result);
+        Assert.AreEqual(expected, result);
     }
 
     [TestMethod]
@@ -60,6 +64,6 @@ public class AboutDay3
 
         var result = new Day3().ProcessPartTwo(input);
 
-        Assert.AreEqual(0L, result);
+        Assert.AreEqual(175659236361660L, result);
     }
 }
